933dc3d2153eb72aad858bedff02d0cdaf0e1c76
Beabkal <b3abkal@gmail.com>
2020-09-22 20:44:39 +0300

    added auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-       
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-<body>
-    
-    </body>
-</html>
+@section('content')
+    <div>
+        <h1>Hello From Layouts</h1>
+    </div>
+@endsection
